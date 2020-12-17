96e4e2f52619f313c80f082956847316b858c27b
Diego Gonzales <dgonzalesj14@unc.edu.pe>
2020-12-17 17:37:39 -0500

    agregando botones para mover la vista del viewPort de virtualScroll

--- src/app/components/virtual-scroll/virtual-scroll.component.ts
+++ src/app/components/virtual-scroll/virtual-scroll.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-virtual-scroll',
@@ -7,12 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VirtualScrollComponent implements OnInit {
 
+  @ViewChild(CdkVirtualScrollViewport) myViewPort: CdkVirtualScrollViewport;
+
   items = Array(500).fill(0);
+
 
   constructor() { }
 
   ngOnInit(): void {
     console.log(this.items);
+  }
+
+  irFinal(): void {
+    this.myViewPort.scrollToIndex(this.items.length);
+  }
+
+  irInicio(): void {
+    this.myViewPort.scrollToIndex(0);
+  }
+
+  irMedio(): void {
+    this.myViewPort.scrollToIndex(this.items.length / 2);
   }
 
 }
